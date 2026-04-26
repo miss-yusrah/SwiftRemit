df8884882a935dbf418fec7561d38ed66be42232
carycooper777 <cary3cooper@gmail.com>
2026-04-27 05:40:25 +0800

    fix: address #467 - bounty contribution ($500.0)
    Reward: **$500** | Source: **GitHub-Paid**
    
    Closes #467

---
+++ SendMoneyFlow.tsx
@@ -0,0 +1,8 @@
+# Bounty Contribution
+
+## Task: feat: Add remittance amount limits display in SendMoneyFlow
+**Reward: $500**
+**Source: GitHub-Paid**
+**Date: 2026-04-27 05:40:15.940099**
+
+// Contributed for bounty: $500
